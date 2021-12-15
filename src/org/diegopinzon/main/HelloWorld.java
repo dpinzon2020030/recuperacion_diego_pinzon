d01590b5bc4927253002a42b6c1f19748f4b3117
Luis Rangel Castro <luisrangelc@gmail.com>
2021-12-14 19:12:06 -0600

    add grid view

--- src/org/diegopinzon/main/HelloWorld.java
+++ src/org/diegopinzon/main/HelloWorld.java
@@ -6,6 +6,8 @@
 package org.diegopinzon.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -19,12 +21,21 @@ public class HelloWorld extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        /*
         Button btn1 = new Button("supreme!!!");
         StackPane root = new StackPane();
         root.getChildren().add(btn1);
         Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.setTitle("First JavaFX Application");
+        stage.show();
+        */
+        
+        Parent root = FXMLLoader.load(getClass().getResource("recuperacion_view.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
         stage.show();
     }
 
